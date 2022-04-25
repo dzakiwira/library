259ba76fb6be4ff0aa502d19edc19251d81ce9df
Dzaki Wiradinata <dzaki.wiradinata@gmail.com>
2022-04-25 14:09:10 -0400

    Function to display myLibrary contents into card form

--- script.js
+++ script.js
@@ -3,15 +3,7 @@ const openModal = document.querySelector('.btn-add');
 const submit = document.querySelector('.submit');
 const form = document.querySelector('.form');
 
-let myLibrary = [
-     {
-        title: 'Dune',
-        author: 'Frank Herbert',
-        pages: 412,
-        read: false
-
-    }
-];
+let myLibrary = [];
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -29,11 +21,59 @@ function getUserBook() {
     let userTitle = document.getElementById('title').value;
     let userAuthor = document.getElementById('author').value;
     let userPages = document.getElementById('pages').value;
-    let UserIsRead = document.getElementById('read').value;
+    let UserIsRead = document.getElementById('read').checked ? 'Read' : 'Not read';
     addBookToLibrary(userTitle, userAuthor, userPages, UserIsRead);
+    updateBookToLibrary();
 }
+
+function updateBookToLibrary() {
+    document.querySelector(".book-container").textContent = "";
+    myLibrary.forEach((book) => {
+      let card = document.createElement("div");
+      let title = document.createElement("div");
+      let author = document.createElement("div");
+      let pages = document.createElement("div");
+      let isRead = document.createElement("div");
+      let removeButton = document.createElement("button");
+  
+      card.classList.add("card");
+  
+      title.classList.add("book-title");
+      title.textContent = `Title: ${book.title}`;
+      card.appendChild(title);
+  
+      author.classList.add("book-author");
+      author.textContent = `Author: ${book.author}`;
+      card.appendChild(author);
+  
+      pages.classList.add("book-pages");
+      pages.textContent = `Pages: ${book.pages}`;
+      card.appendChild(pages);
+      
+      isRead.classList.add("book-isRead");
+      isRead.textContent = book.read;
+      card.appendChild(isRead);
+  
+      removeButton.classList.add("remove-btn");
+      removeButton.textContent = "Remove";
+      card.appendChild(removeButton);
+  
+      removeButton.setAttribute("data", book.indexOfBook);
+  
+      removeButton.addEventListener("click", (e) => {
+        myLibrary.splice(removeButton.dataset.data, 1);
+        updateBookToLibrary();
+      });
+      document.querySelector(".book-container").appendChild(card);
+    });
+  }
 
 openModal.addEventListener('click', () => {
     form.reset();
     modal.showModal();
 })
+
+let newBook = new Book('Dune', 'Frank Herbert', 421, 'Read');
+myLibrary.push(newBook);
+
+updateBookToLibrary();
